eaf51e46f3b7958ec931d0994dbb35aa937c6c5d
Irfankhan20 <sipathan96@gmail.com>
2025-02-11 19:51:18 +0600

    show latest blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+import { RiH1 } from "react-icons/ri";
+
 const HomePage = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
   return (
     <>
-      <h1 className="text-center text-4xl my-5">Latest Blogs</h1>
+      <LatestBlogs blogs={blogs}></LatestBlogs>
     </>
   );
 };
